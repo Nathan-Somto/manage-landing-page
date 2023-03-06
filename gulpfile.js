f08de72bebede1ef8af4610f3501e8a61bedd194
Mkparu <mkparusomtochi26@gmail.com>
2023-03-06 21:44:32 +0100

    finished the html

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+function buildStyles(){
+    return gulp.src('./styles/**/*.scss')
+    .pipe(sass().on('error',sass.logError))
+    .pipe(gulp.dest('index.css'));
+}
+exports.buildStyles = buildStyles;
+exports.watch = function(){
+    gulp.watch('./styles/**/*.scss',['sass']);
+}
---
+++ index.js
@@ -0,0 +1,3 @@
+const dateSpan = document.getElementById('js--display--year');
+const date = new Date();
+dateSpan.innerHTML = date.getFullYear();
